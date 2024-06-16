1a9ae4b0b35f953868c85d9553ef76df86559f1b
HP657 <junlee5070@gmail.com>
2024-06-17 00:34:37 +0900

    useEffect에서 특정부분이 바뀌었을떄만 수정할수 있게 해둠

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,28 +1,47 @@
-import Button from "./Button";
-import styles from "./App.module.css"
 import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => {
     setValue((prev) => prev + 1);
   }
-  console.log("i run all the time");
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  }
   useEffect(() => {
-    console.log("Call the API")
-  }, [])
+    console.log("Run only once")
+  }, []);
+  useEffect(() => {
+    if (keyword !== "") {
+      console.log("Search for " + keyword);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    if (counter !== 0){
+      console.log("Counter " + counter)
+    }
+  }, [counter]);
+  useEffect(() => {
+    if (counter !== 0 || keyword !== "") {
+      console.log("Both");
+    }
+  }, [keyword, counter]);
   return (
     <div>
-      <div>
-        <h1>{counter}</h1>
-        <button onClick={onClick}>Click Me</button>
-      </div>
-      <div>
-        <h1 className={styles.title}>Welcome Back!</h1>
-        <Button text={"Continue"}/>
-      </div>
+      <input 
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 }
-
+{/* <div>
+  <h1 className={styles.title}>Welcome Back!</h1>
+  <Button text={"Continue"}/>
+</div> */}
 export default App;
