73518a7544f20086aa9ead25b7d6b32fa4cfd99e
HP657 <junlee5070@gmail.com>
2024-06-16 19:37:20 +0900

    첫 렌더링 때에만 실행 가능케 하는 useEffect 사용 해봄

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,26 @@
 import Button from "./Button";
 import styles from "./App.module.css"
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => {
+    setValue((prev) => prev + 1);
+  }
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("Call the API")
+  }, [])
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"Continue"}/>
+      <div>
+        <h1>{counter}</h1>
+        <button onClick={onClick}>Click Me</button>
+      </div>
+      <div>
+        <h1 className={styles.title}>Welcome Back!</h1>
+        <Button text={"Continue"}/>
+      </div>
     </div>
   );
 }
