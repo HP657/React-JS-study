8fe44293e63fbd151f9180ff616ac5839df9f903
HP657 <junlee5070@gmail.com>
2024-07-02 23:47:48 +0900

    무비 컴포넌트 생성

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,16 +1,18 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Moive from './Movie';
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
 
-  const getMovies = async() => {
-    const url = "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year";
-    const res = await axios.get(url)
+  const getMovies = async () => {
+    const url =
+      'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year';
+    const res = await axios.get(url);
     setMovies(res.data.data.movies);
     setLoading(false);
-  }
+  };
 
   useEffect(() => {
     getMovies();
@@ -23,16 +25,13 @@ function App() {
       ) : (
         <div>
           {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
+            <Moive
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
           ))}
         </div>
       )}
---
+++ movie/src/Movie.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+function Moive({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Moive.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Moive;
